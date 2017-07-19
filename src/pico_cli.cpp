0c091698be42e62d81c90fa88afb0edfc039209e
Keshav Sharma <sharmakeshav15157@gmail.com>
2017-07-19 12:57:57 +0530

    Update pico_cli.cpp

--- src/pico_cli.cpp
+++ src/pico_cli.cpp
@@ -1,41 +1,9 @@
-//
-#include "stdafx.h" 
+#include "stdafx.h" //if using visualStudio, Pre-Compiled headerfile option is not used. 
 using namespace std;
-//int ID_valid();
 int main()
 {	
-	/*
-	if (!ID_valid()) {
-		cout << "Invalid !";
-		cin.get();
-		return 1;
-	}
-	*/
 	cmdEngine *engine = new cmdEngine;
 	engine->start_cmdEngine(); //starts the command Line Engine
 	delete engine;
 	return 0;
 }
-/*
-int ID_valid() {
-	string pass;
-	int attempts = 2;
-	cout << "Hello Keshav Sharma \n";
-	do {
-		cout << "Enter Security Key : ";
-		getline(cin, pass);
-		if (pass == "keshav15157") {
-			cout << "Valid Key\n";
-			cin.get();
-			break;
-		}
-		else {
-			cout << "\n Invalid ! Attempts left : " << attempts-1 << endl;
-			if (attempts - 1 == 0)
-				return 0;
-		}
-	} while (attempts--);
-
-	return 1;
-}
-*/
